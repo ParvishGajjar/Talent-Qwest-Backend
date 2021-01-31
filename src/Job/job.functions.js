87fffbb6106410835a8e803c9c037b52c7d826ed
ParvishGajjar <parvishgajjar@gmail.com>
2021-01-31 15:31:13 +0530

    did split skill in backend

--- src/Job/job.functions.js
+++ src/Job/job.functions.js
@@ -1,18 +1,25 @@
 import { query } from "../index";
+import { split } from "lodash";
 
 export const applyForJob = async (req, res) => {
   try {
-    const alreadyapplied= await query(`select * from user_job where job_id=${req.params.jobId} and user_id=${req.user[0].id}`)
-    if(alreadyapplied[0]){
-        return res.status(400).json({data: false,message:`Already applied`, status: false})
+    const alreadyapplied = await query(
+      `select * from user_job where job_id=${req.params.jobId} and user_id=${req.user[0].id}`
+    );
+    if (alreadyapplied[0]) {
+      return res
+        .status(400)
+        .json({ data: false, message: `Already applied`, status: false });
     }
     const result = await query(
       `insert into user_job values (${req.params.jobId},${req.user[0].id})`
     );
     if (result.affectedRows) {
-      return res
-        .status(200)
-        .json({ data: true, message: `Successfully applied for job`, status: true });
+      return res.status(200).json({
+        data: true,
+        message: `Successfully applied for job`,
+        status: true,
+      });
     } else {
       return res
         .status(400)
@@ -41,6 +48,9 @@ export const openJob = async (req, res) => {
     and 
     job_post.id not in (select user_job.job_id from user_job where user_job.user_id = ${req.user[0].id})
     group by job_post.id;`);
+    result.forEach((value) => {
+      value.Required_Skills = split(value.Required_Skills, "|");
+    });
     if (result[0]) {
       return res
         .status(200)
@@ -61,8 +71,8 @@ export const openJob = async (req, res) => {
 };
 
 export const closedJob = async (req, res) => {
-    try {
-      const result = await query(`select job_post.id, job_post.name, job_post.description, 
+  try {
+    const result = await query(`select job_post.id, job_post.name, job_post.description, 
           job_post.salary, job_post.vacancy, job_post.timestamp, job_post.is_open,
           group_concat(coding_list.name separator '|') as 'Required_Skills' 
           from job_post 
@@ -73,28 +83,31 @@ export const closedJob = async (req, res) => {
           and 
           job_post.id in (select user_job.job_id from user_job where user_job.user_id = ${req.user[0].id})
           group by job_post.id;`);
-      if (result[0]) {
-        return res
-          .status(200)
-          .json({ data: result, message: `fetched closed jobs`, status: true });
-      } else {
-        return res
-          .status(404)
-          .json({ data: [], message: `No closed jobs found`, status: true });
-      }
-    } catch (e) {
-      console.log(e);
-      return res.status(400).json({
-        data: false,
-        message: `fail`,
-        status: false,
-      });
+    result.forEach((value) => {
+      value.Required_Skills = split(value.Required_Skills, "|");
+    });
+    if (result[0]) {
+      return res
+        .status(200)
+        .json({ data: result, message: `fetched closed jobs`, status: true });
+    } else {
+      return res
+        .status(404)
+        .json({ data: [], message: `No closed jobs found`, status: true });
     }
-  };
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({
+      data: false,
+      message: `fail`,
+      status: false,
+    });
+  }
+};
 
-  export const appliedJob = async (req, res) => {
-    try {
-      const result = await query(`select job_post.id, job_post.name, job_post.description, 
+export const appliedJob = async (req, res) => {
+  try {
+    const result = await query(`select job_post.id, job_post.name, job_post.description, 
           job_post.salary, job_post.vacancy, job_post.timestamp, job_post.is_open,
           group_concat(coding_list.name separator '|') as 'Required_Skills' 
           from job_post 
@@ -105,21 +118,24 @@ export const closedJob = async (req, res) => {
           and 
           job_post.id in (select user_job.job_id from user_job where user_job.user_id = ${req.user[0].id})
           group by job_post.id;`);
-      if (result[0]) {
-        return res
-          .status(200)
-          .json({ data: result, message: `fetched applied jobs`, status: true });
-      } else {
-        return res
-          .status(404)
-          .json({ data: [], message: `No job applied yet`, status: true });
-      }
-    } catch (e) {
-      console.log(e);
-      return res.status(400).json({
-        data: false,
-        message: `fail`,
-        status: false,
-      });
+    result.forEach((value) => {
+      value.Required_Skills = split(value.Required_Skills, "|");
+    });
+    if (result[0]) {
+      return res
+        .status(200)
+        .json({ data: result, message: `fetched applied jobs`, status: true });
+    } else {
+      return res
+        .status(404)
+        .json({ data: [], message: `No job applied yet`, status: true });
     }
-  };
+  } catch (e) {
+    console.log(e);
+    return res.status(400).json({
+      data: false,
+      message: `fail`,
+      status: false,
+    });
+  }
+};
